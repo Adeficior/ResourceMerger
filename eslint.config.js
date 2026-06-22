a0a546937a8c81626f7a06cf7b182539bf33e881
Niklas Widmann <nik@somethingcatchy.net>
2026-06-22 14:30:32 +0200

    use eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+//@ts-check
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import ts from "typescript-eslint";
+
+export default defineConfig([
+  js.configs.recommended,
+  ts.configs.recommended,
+  {
+    rules: {
+      "no-console": "error",
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/consistent-type-exports": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+        projectService: true,
+      },
+    },
+  },
+]);
