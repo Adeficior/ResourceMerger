040242c89addb370b5784b5cd8ed3041caa6974d
Niklas Widmann <nik@somethingcatchy.net>
2026-07-02 12:47:53 +0200

    update pack-resolver

--- src/create.ts
+++ src/create.ts
@@ -1,36 +1,69 @@
 import {
   afterFinalize,
   distributedAcceptor,
+  extendContext,
   simpleAcceptor,
+  supplyAcceptorContext,
   type Acceptable,
   type Acceptor,
+  type ContextLike,
 } from "@adeficior/pack-resolver";
 import type { MergeStrategy } from "./strategy/index.js";
 import LangMerger from "./strategy/lang.js";
 import ModelMerger from "./strategy/models.js";
 import TagMerger from "./strategy/tags.js";
 
-export function mergingAcceptor(
-  acceptor: Acceptor,
-  strategy: MergeStrategy,
-): Acceptor {
-  const cache = new Map<string, Acceptable>();
+type WithSources<T extends ContextLike> = Omit<T, "source"> & {
+  source: string[];
+};
 
-  const transformer = simpleAcceptor(async (path, data) => {
-    const content = await data;
-    const cached = cache.get(path);
-    if (cached) {
-      const merged = strategy.merge(cached, content);
-      cache.set(path, merged);
-    } else {
-      cache.set(path, content);
-    }
-  });
+function sourceOf(context: ContextLike): string[] {
+  if ("source" in context) {
+    if (typeof context.source === "string") return [context.source];
+    if (Array.isArray(context.source))
+      return context.source.filter((it) => typeof it === "string");
+  }
+
+  return [];
+}
+
+export function mergingAcceptor<Context extends ContextLike>(
+  acceptor: Acceptor<Acceptable, WithSources<Context>>,
+  strategy: MergeStrategy,
+): Acceptor<Acceptable, Context> {
+  const cache = new Map<
+    string,
+    { data: Acceptable; context: WithSources<Context> }
+  >();
+
+  const transformer = simpleAcceptor<Acceptable, Context>(
+    async (path, content, context) => {
+      const data = await content;
+      const cached = cache.get(path);
+      const sources = sourceOf(context);
+
+      if (cached) {
+        const merged = strategy.merge(cached.data, data);
+        // TODO combined context somehow?
+        cache.set(path, {
+          data: merged,
+          context: extendContext(cached.context, {
+            source: [...cached.context.source, ...sources],
+          }),
+        });
+      } else {
+        cache.set(path, {
+          data,
+          context: extendContext(context, { source: sources }),
+        });
+      }
+    },
+  );
 
   return afterFinalize(transformer, async () => {
     await Promise.all(
-      [...cache].map(([path, data]) => {
-        acceptor.accept(path, Promise.resolve(data));
+      [...cache].map(([path, { data, context }]) => {
+        acceptor.accept(path, Promise.resolve(data), context);
       }),
     );
 
@@ -38,8 +71,12 @@ export function mergingAcceptor(
   });
 }
 
-export function createMergingAcceptor(acceptor: Acceptor): Acceptor {
-  const withoutFinalize: Acceptor = { accept: acceptor.accept };
+export function createMergingAcceptor<Context extends ContextLike>(
+  acceptor: Acceptor<Acceptable, WithSources<Context>>,
+): Acceptor<Acceptable, Context> {
+  const withoutFinalize: typeof acceptor = {
+    accept: acceptor.accept,
+  };
   const merging = distributedAcceptor(
     {
       "assets/*/models/**/*.json": mergingAcceptor(
@@ -49,7 +86,7 @@ export function createMergingAcceptor(acceptor: Acceptor): Acceptor {
       "assets/*/lang/**/*.json": mergingAcceptor(withoutFinalize, LangMerger),
       "data/*/tags/**/*.json": mergingAcceptor(withoutFinalize, TagMerger),
     },
-    withoutFinalize,
+    supplyAcceptorContext(withoutFinalize, { source: [] }),
   );
 
   if (acceptor.finalize) return afterFinalize(merging, acceptor.finalize);
